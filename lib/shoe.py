6f2fbe3337da1d767f4203f9ec9b9ebcf8f23404
alex kamau <alex.kamau1@student.moringaschool.com>
2025-08-29 15:18:53 -0700

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            return
+        if value <= 0:
+            raise ValueError("size must be positive")
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"          # create the attribute
+        print("Your shoe is as good as new!")  # print the exact message
